ab5b327b28d9018180f99461e7aab750d3e538f7
Indyuce <cym.peyrat@yahoo.fr>
2021-08-21 17:41:55 +0200

    phenomenologically fixed loading error

--- src/main/java/net/Indyuce/inventory/MMOInventory.java
+++ src/main/java/net/Indyuce/inventory/MMOInventory.java
@@ -4,7 +4,9 @@ import net.Indyuce.inventory.command.MMOInventoryCommand;
 import net.Indyuce.inventory.command.MMOInventoryCompletion;
 import net.Indyuce.inventory.compat.InventoryUpdater;
 import net.Indyuce.inventory.compat.mmoitems.MMOItemsCompatibility;
-import net.Indyuce.inventory.gui.PlayerInventoryView;
+import net.Indyuce.inventory.compat.mmoitems.MMOItemsLevelRestriction;
+import net.Indyuce.inventory.compat.mmoitems.MMOItemsTypeRestriction;
+import net.Indyuce.inventory.compat.mmoitems.MMOItemsUniqueRestriction;
 import net.Indyuce.inventory.inventory.SimpleInventoryHandler;
 import net.Indyuce.inventory.listener.*;
 import net.Indyuce.inventory.manager.DataManager;
@@ -20,7 +22,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.InventoryView;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -28,150 +29,155 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.logging.Level;
 
 public class MMOInventory extends JavaPlugin implements Listener {
-	public static MMOInventory plugin;
+    public static MMOInventory plugin;
 
-	private final SlotManager slotManager = new SlotManager();
+    private final SlotManager slotManager = new SlotManager();
 
-	/**
-	 * See {@link InventoryUpdater} for explanation. This is
-	 * the list of all the plugins which require inventory updates.
-	 */
-	private final List<InventoryUpdater> inventoryUpdaters = new ArrayList<>();
+    /**
+     * See {@link InventoryUpdater} for explanation. This is
+     * the list of all the plugins which require inventory updates.
+     */
+    private final List<InventoryUpdater> inventoryUpdaters = new ArrayList<>();
 
-	private DataManager dataManager;
-	private ServerVersion version;
-	private VersionWrapper versionWrapper;
-	private ConfigFile language;
+    private DataManager dataManager;
+    private ServerVersion version;
+    private VersionWrapper versionWrapper;
+    private ConfigFile language;
 
-	// Cached config options
-	public int inventorySlots;
+    // Cached config options
+    public int inventorySlots;
 
-	public void onLoad() {
-		plugin = this;
-	}
+    public void onLoad() {
+        plugin = this;
 
-	public void onEnable() {
+        if (Bukkit.getPluginManager().getPlugin("MMOItems") != null) {
+            slotManager.registerRestriction(MMOItemsTypeRestriction::new, "mmoitemstype", "mmoitemtype", "mitype");
+            slotManager.registerRestriction(config -> new MMOItemsLevelRestriction(), "mmoitemslevel", "mmoitemlevel", "milevel");
+            slotManager.registerRestriction(MMOItemsUniqueRestriction::new, "unique");
+        }
+    }
 
-		try {
-			version = new ServerVersion(Bukkit.getServer().getClass());
-			getLogger().log(Level.INFO, "Detected Bukkit Version: " + version.toString());
-			versionWrapper = (VersionWrapper) Class.forName("net.Indyuce.inventory.version.wrapper.VersionWrapper_" + version.toString().substring(1))
-					.newInstance();
-		} catch (Exception e) {
-			getLogger().log(Level.INFO, "Your server version is handled via reflection");
-			versionWrapper = new VersionWrapper_Reflection();
-		}
+    public void onEnable() {
 
-		saveDefaultConfig();
-		saveDefaultFile("language");
-		saveDefaultFile("items");
-		reload();
+        try {
+            version = new ServerVersion(Bukkit.getServer().getClass());
+            getLogger().log(Level.INFO, "Detected Bukkit Version: " + version.toString());
+            versionWrapper = (VersionWrapper) Class.forName("net.Indyuce.inventory.version.wrapper.VersionWrapper_" + version.toString().substring(1))
+                    .newInstance();
+        } catch (Exception e) {
+            getLogger().log(Level.INFO, "Your server version is handled via reflection");
+            versionWrapper = new VersionWrapper_Reflection();
+        }
 
-		Bukkit.getServer().getPluginManager().registerEvents(this, this);
-		Bukkit.getServer().getPluginManager().registerEvents(new GuiListener(), this);
-		Bukkit.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+        saveDefaultConfig();
+        saveDefaultFile("language");
+        saveDefaultFile("items");
+        reload();
 
-		// MySQL or YAML
-		dataManager = getConfig().getBoolean("mysql.enabled") ? new SQLDataManager() : new YamlDataManager();
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        Bukkit.getServer().getPluginManager().registerEvents(new GuiListener(), this);
+        Bukkit.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
 
-		if (getConfig().getBoolean("resource-pack.enabled"))
-			Bukkit.getServer().getPluginManager().registerEvents(new ResourcePack(getConfig().getConfigurationSection("resource-pack")), this);
+        // MySQL or YAML
+        dataManager = getConfig().getBoolean("mysql.enabled") ? new SQLDataManager() : new YamlDataManager();
 
-		if (getConfig().getBoolean("no-custom-inventory")) {
-			dataManager.setInventoryProvider(SimpleInventoryHandler::new);
-			Bukkit.getPluginManager().registerEvents(new NoCustomInventory(), this);
-		} else {
+        if (getConfig().getBoolean("resource-pack.enabled"))
+            Bukkit.getServer().getPluginManager().registerEvents(new ResourcePack(getConfig().getConfigurationSection("resource-pack")), this);
 
-			if (getConfig().getBoolean("save-on-leave"))
-				Bukkit.getPluginManager().registerEvents(new SaveOnLeave(), this);
+        if (getConfig().getBoolean("no-custom-inventory")) {
+            dataManager.setInventoryProvider(SimpleInventoryHandler::new);
+            Bukkit.getPluginManager().registerEvents(new NoCustomInventory(), this);
+        } else {
 
-			if (getConfig().getBoolean("inventory-button.enabled"))
-				Bukkit.getPluginManager().registerEvents(new InventoryButtonListener(getConfig().getConfigurationSection("inventory-button")), this);
+            if (getConfig().getBoolean("save-on-leave"))
+                Bukkit.getPluginManager().registerEvents(new SaveOnLeave(), this);
 
-			if (getConfig().getBoolean("drop-on-death"))
-				Bukkit.getServer().getPluginManager().registerEvents(new DeathDrops(), this);
-		}
+            if (getConfig().getBoolean("inventory-button.enabled"))
+                Bukkit.getPluginManager().registerEvents(new InventoryButtonListener(getConfig().getConfigurationSection("inventory-button")), this);
 
-		getCommand("mmoinventory").setExecutor(new MMOInventoryCommand());
-		getCommand("mmoinventory").setTabCompleter(new MMOInventoryCompletion());
+            if (getConfig().getBoolean("drop-on-death"))
+                Bukkit.getServer().getPluginManager().registerEvents(new DeathDrops(), this);
+        }
 
-		// /reload friendly
-		Bukkit.getOnlinePlayers().forEach(dataManager::setupData);
+        getCommand("mmoinventory").setExecutor(new MMOInventoryCommand());
+        getCommand("mmoinventory").setTabCompleter(new MMOInventoryCompletion());
 
-		if (Bukkit.getPluginManager().getPlugin("MMOItems") != null) {
-			new MMOItemsCompatibility();
-			getLogger().log(Level.INFO, "Hooked onto MMOItems");
-		}
-	}
+        // /reload friendly
+        Bukkit.getOnlinePlayers().forEach(dataManager::setupData);
 
-	public void onDisable() {
-		dataManager.save();
-	}
+        if (Bukkit.getPluginManager().getPlugin("MMOItems") != null) {
+            new MMOItemsCompatibility();
+            getLogger().log(Level.INFO, "Hooked onto MMOItems");
+        }
+    }
 
-	public void reload() {
-		reloadConfig();
-		language = new ConfigFile("language");
-		slotManager.reload();
+    public void onDisable() {
+        dataManager.save();
+    }
 
-		try {
-			inventorySlots = getConfig().getInt("inventory-slots");
-			Validate.isTrue(inventorySlots > 0 && inventorySlots < 55, "Number must be greater than 9 and lower than 54");
-			Validate.isTrue(inventorySlots % 9 == 0, "Number must be a multiple of 9");
+    public void reload() {
+        reloadConfig();
+        language = new ConfigFile("language");
+        slotManager.reload();
 
-		} catch (IllegalArgumentException exception) {
-			inventorySlots = 36;
-			getLogger().log(Level.WARNING, "Invalid inventory slot number: " + exception.getMessage());
-		}
-	}
+        try {
+            inventorySlots = getConfig().getInt("inventory-slots");
+            Validate.isTrue(inventorySlots > 0 && inventorySlots < 55, "Number must be greater than 9 and lower than 54");
+            Validate.isTrue(inventorySlots % 9 == 0, "Number must be a multiple of 9");
 
-	public VersionWrapper getVersionWrapper() {
-		return versionWrapper;
-	}
+        } catch (IllegalArgumentException exception) {
+            inventorySlots = 36;
+            getLogger().log(Level.WARNING, "Invalid inventory slot number: " + exception.getMessage());
+        }
+    }
 
-	public DataManager getDataManager() {
-		return dataManager;
-	}
+    public VersionWrapper getVersionWrapper() {
+        return versionWrapper;
+    }
 
-	public SlotManager getSlotManager() {
-		return slotManager;
-	}
+    public DataManager getDataManager() {
+        return dataManager;
+    }
 
-	public ServerVersion getVersion() {
-		return version;
-	}
+    public SlotManager getSlotManager() {
+        return slotManager;
+    }
 
-	public void registerInventoryUpdater(InventoryUpdater updater) {
-		Validate.notNull(updater, "Updater cannot be null");
+    public ServerVersion getVersion() {
+        return version;
+    }
 
-		inventoryUpdaters.add(updater);
-	}
+    public void registerInventoryUpdater(InventoryUpdater updater) {
+        Validate.notNull(updater, "Updater cannot be null");
 
-	/**
-	 * Iterates through all registered {@link InventoryUpdater} and
-	 * updates the player for every plugin that needs it.
-	 *
-	 * @param player Player which inventory requires an update
-	 */
-	public void updateInventory(Player player) {
-		for (InventoryUpdater updater : inventoryUpdaters)
-			updater.updateInventory(player);
-	}
+        inventoryUpdaters.add(updater);
+    }
 
-	public String getTranslation(String path) {
-		return ChatColor.translateAlternateColorCodes('&', language.getConfig().getString(path));
-	}
+    /**
+     * Iterates through all registered {@link InventoryUpdater} and
+     * updates the player for every plugin that needs it.
+     *
+     * @param player Player which inventory requires an update
+     */
+    public void updateInventory(Player player) {
+        for (InventoryUpdater updater : inventoryUpdaters)
+            updater.updateInventory(player);
+    }
 
-	private void saveDefaultFile(String path) {
-		try {
-			File file = new File(getDataFolder(), path + ".yml");
-			if (!file.exists())
-				Files.copy(getResource("default/" + path + ".yml"), file.getAbsoluteFile().toPath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public String getTranslation(String path) {
+        return ChatColor.translateAlternateColorCodes('&', language.getConfig().getString(path));
+    }
+
+    private void saveDefaultFile(String path) {
+        try {
+            File file = new File(getDataFolder(), path + ".yml");
+            if (!file.exists())
+                Files.copy(getResource("default/" + path + ".yml"), file.getAbsoluteFile().toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/net/Indyuce/inventory/manager/SlotManager.java
+++ src/main/java/net/Indyuce/inventory/manager/SlotManager.java
@@ -1,16 +1,12 @@
 package net.Indyuce.inventory.manager;
 
 import net.Indyuce.inventory.MMOInventory;
-import net.Indyuce.inventory.util.ConfigFile;
-import net.Indyuce.inventory.util.LineConfig;
 import net.Indyuce.inventory.slot.CustomSlot;
 import net.Indyuce.inventory.slot.SlotRestriction;
 import net.Indyuce.inventory.slot.SlotType;
-import net.Indyuce.inventory.compat.mmoitems.MMOItemsLevelRestriction;
-import net.Indyuce.inventory.compat.mmoitems.MMOItemsTypeRestriction;
-import net.Indyuce.inventory.compat.mmoitems.MMOItemsUniqueRestriction;
+import net.Indyuce.inventory.util.ConfigFile;
+import net.Indyuce.inventory.util.LineConfig;
 import org.apache.commons.lang.Validate;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -26,131 +22,124 @@ import java.util.stream.Collectors;
 
 public class SlotManager {
 
-	/**
-	 * Custom slot instances saved using bukkit inventory slot index
-	 */
-	private final Map<Integer, CustomSlot> slots = new HashMap<>();
+    /**
+     * Custom slot instances saved using bukkit inventory slot index
+     */
+    private final Map<Integer, CustomSlot> slots = new HashMap<>();
 
-	/**
-	 * Used to register extra slot restrictions from external plugins
-	 */
-	private final Map<String, Function<LineConfig, SlotRestriction>> restrictionLoader = new HashMap<>();
+    /**
+     * Used to register extra slot restrictions from external plugins
+     */
+    private final Map<String, Function<LineConfig, SlotRestriction>> restrictionLoader = new HashMap<>();
 
-	/**
-	 * Used to fill up inventory space. Fillers are not registered in the slot
-	 * map but instead cached in this field to both have easier access AND make
-	 * them not interfere with other systems as they aren't real slots
-	 */
-	private CustomSlot fill = new CustomSlot("FILL", "", SlotType.FILL, -1, new ItemStack(Material.AIR));
+    /**
+     * Used to fill up inventory space. Fillers are not registered in the slot
+     * map but instead cached in this field to both have easier access AND make
+     * them not interfere with other systems as they aren't real slots
+     */
+    private CustomSlot fill = new CustomSlot("FILL", "", SlotType.FILL, -1, new ItemStack(Material.AIR));
 
-	public SlotManager() {
+    private boolean registration = true;
 
-		if (Bukkit.getPluginManager().getPlugin("MMOItems") != null) {
-			registerRestriction(MMOItemsTypeRestriction::new, "mmoitemstype", "mmoitemtype", "mitype");
-			registerRestriction(config -> new MMOItemsLevelRestriction(), "mmoitemslevel", "mmoitemlevel", "milevel");
-			registerRestriction(MMOItemsUniqueRestriction::new, "unique");
-		}
-	}
+    /**
+     * Registers a custom slot that will appear in /rpginv. Filler items are not
+     * registered in the map
+     *
+     * @param slot The slot to register
+     */
+    public void register(CustomSlot slot) {
+        Validate.isTrue(!slots.containsKey(slot.getIndex()), "A slot with the same index already exists");
 
-	/**
-	 * Registers a custom slot that will appear in /rpginv. Filler items are not
-	 * registered in the map
-	 *
-	 * @param slot The slot to register
-	 */
-	public void register(CustomSlot slot) {
-		Validate.isTrue(!slots.containsKey(slot.getIndex()), "A slot with the same index already exists");
+        // only register if not filler
+        if (slot.getType() == SlotType.FILL)
+            fill = slot;
+        else
+            slots.put(slot.getIndex(), slot);
+    }
 
-		// only register if not filler
-		if (slot.getType() == SlotType.FILL)
-			fill = slot;
-		else
-			slots.put(slot.getIndex(), slot);
-	}
+    public void unregister(int index) {
+        slots.remove(index);
+    }
 
-	public void unregister(int index) {
-		slots.remove(index);
-	}
+    /**
+     * Matches a custom slot with a GUI slot index
+     *
+     * @param index The GUI slot index
+     * @return The corresponding custom slot or null if it could not be found
+     */
+    public CustomSlot get(int index) {
+        return slots.getOrDefault(index, null);
+    }
 
-	/**
-	 * Matches a custom slot with a GUI slot index
-	 * 
-	 * @param index
-	 *            The GUI slot index
-	 * @return The corresponding custom slot or null if it could not be found
-	 */
-	public CustomSlot get(int index) {
-		return slots.getOrDefault(index, null);
-	}
+    public Collection<CustomSlot> getLoaded() {
+        return slots.values();
+    }
 
-	public Collection<CustomSlot> getLoaded() {
-		return slots.values();
-	}
+    /**
+     * Reads a slot restriction from a config file or throws an IAE if it could
+     * not find any which matches
+     *
+     * @param config Life config to read from
+     * @return Slot restriction if found
+     */
+    public SlotRestriction readRestriction(LineConfig config) {
+        String id = config.getKey().toLowerCase().replace("-", "_").replace(" ", "_");
 
-	/**
-	 * Reads a slot restriction from a config file or throws an IAE if it could
-	 * not find any which matches
-	 * 
-	 * @param config
-	 *            Life config to read from
-	 * @return Slot restriction if found
-	 */
-	public SlotRestriction readRestriction(LineConfig config) {
-		String id = config.getKey().toLowerCase().replace("-", "_").replace(" ", "_");
+        for (String check : restrictionLoader.keySet())
+            if (check.equals(id))
+                return restrictionLoader.get(check).apply(config);
 
-		for (String check : restrictionLoader.keySet())
-			if (check.equals(id))
-				return restrictionLoader.get(check).apply(config);
+        throw new IllegalArgumentException("Could not find restriction with ID " + config.getKey());
+    }
 
-		throw new IllegalArgumentException("Could not find restriction with ID " + config.getKey());
-	}
+    /**
+     * Registers a slot restriction in MMOInventory
+     *
+     * @param function A loader function which takes into input a line config and
+     *                 outputs the right slot restriction instance
+     * @param ids      The line config keys which will be assigned to this slot
+     *                 restriction
+     */
+    public void registerRestriction(Function<LineConfig, SlotRestriction> function, String... ids) {
+        Validate.isTrue(registration, "Slot restriction registration is disabled");
+        Validate.notNull(function, "Function must not be null.");
 
-	/**
-	 * Registers a slot restriction in MMOInventory
-	 * 
-	 * @param function
-	 *            A loader function which takes into input a line config and
-	 *            outputs the right slot restriction instance
-	 * @param ids
-	 *            The line config keys which will be assigned to this slot
-	 *            restriction
-	 */
-	public void registerRestriction(Function<LineConfig, SlotRestriction> function, String... ids) {
-		Validate.notNull(function, "Function must not be null.");
+        for (String id : ids) {
+            id = id.toLowerCase().replace("-", "_").replace(" ", "_");
+            Validate.isTrue(!restrictionLoader.containsKey(id), "Database already contains a restriction with ID " + id);
+            restrictionLoader.put(id, function);
+        }
+    }
 
-		for (String id : ids) {
-			id = id.toLowerCase().replace("-", "_").replace(" ", "_");
-			Validate.isTrue(!restrictionLoader.containsKey(id), "Database already contains a restriction with ID " + id);
-			restrictionLoader.put(id, function);
-		}
-	}
+    public CustomSlot getFiller() {
+        return fill;
+    }
 
-	public CustomSlot getFiller() {
-		return fill;
-	}
+    public void reload() {
+        if (registration)
+            registration = false;
+        else
+            slots.clear();
 
-	public void reload() {
-		slots.clear();
+        FileConfiguration config = new ConfigFile("items").getConfig();
+        for (String key : config.getKeys(false))
+            try {
+                ConfigurationSection section = config.getConfigurationSection(key);
+                Validate.notNull(section, "Could not read config section");
 
-		FileConfiguration config = new ConfigFile("items").getConfig();
-		for (String key : config.getKeys(false))
-			try {
-				ConfigurationSection section = config.getConfigurationSection(key);
-				Validate.notNull(section, "Could not read config section");
+                register(new CustomSlot(section));
+            } catch (IllegalArgumentException exception) {
+                MMOInventory.plugin.getLogger().log(Level.WARNING, "Could not load slot " + key + ": " + exception.getMessage());
+            }
 
-				register(new CustomSlot(section));
-			} catch (IllegalArgumentException exception) {
-				MMOInventory.plugin.getLogger().log(Level.WARNING, "Could not load slot " + key + ": " + exception.getMessage());
-			}
+        MMOInventory.plugin.getLogger().log(Level.INFO, "Successfully registered " + slots.size() + " inventory slots.");
+    }
 
-		MMOInventory.plugin.getLogger().log(Level.INFO, "Successfully registered " + slots.size() + " inventory slots.");
-	}
-
-	/**
-	 * @return A set of all the registered custom slots without filler items and
-	 *         vanilla item slots
-	 */
-	public Set<CustomSlot> getCustomSlots() {
-		return getLoaded().stream().filter(slot -> slot.getType() == SlotType.ACCESSORY).collect(Collectors.toSet());
-	}
+    /**
+     * @return A set of all the registered custom slots without filler items and
+     *         vanilla item slots
+     */
+    public Set<CustomSlot> getCustomSlots() {
+        return getLoaded().stream().filter(slot -> slot.getType() == SlotType.ACCESSORY).collect(Collectors.toSet());
+    }
 }
